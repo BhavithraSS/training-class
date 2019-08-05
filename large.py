5436a7d317d4e3edc25f158869d199190fbcd41a
KeerthiGL <53601492+KeerthiGL@users.noreply.github.com>
2019-08-05 12:19:34 +0530

    Create large.py

---
+++ large.py
@@ -0,0 +1,9 @@
+a=int(input("input your value"))
+b=int(input("input your value"))
+c=int(input("input your value"))
+if a>b and a>c:
+ print("a is greater value")
+elif b>c and b>a:
+ print("b is greater value")
+else :
+ print("c is greater value")
