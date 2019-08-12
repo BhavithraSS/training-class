c5abe9c15af0dbcfc38bc4b8b9d51f372199827b
KeerthiGL <53601492+KeerthiGL@users.noreply.github.com>
2019-08-12 00:11:11 -0900

    Create summing odd.py

---
+++ summing odd.py
@@ -0,0 +1,2 @@
+for idx in range(10): s = 0 if idx == 0 else s = [(i % 2) for i in range(10)][idx] * range(10)[idx] + s
+for idx in range(10): s = 0 if idx == 0 else s = [(i % 2) for i in range(10)][idx] * range(10)[idx] + s
